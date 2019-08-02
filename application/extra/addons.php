b58b72666bb2b934e9de61059bd3153002e71791
wlt <wlt5870@163.com>
2019-08-02 11:49:23 +0800

    init

---
+++ application/extra/addons.php
@@ -0,0 +1,44 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'user_sidenav_after' => 
+    array (
+      0 => 'invite',
+    ),
+    'user_register_successed' => 
+    array (
+      0 => 'invite',
+    ),
+    'leesignhook' => 
+    array (
+      0 => 'leesign',
+    ),
+    'config_init' => 
+    array (
+      0 => 'qcloudsms',
+    ),
+    'sms_send' => 
+    array (
+      0 => 'qcloudsms',
+    ),
+    'sms_notice' => 
+    array (
+      0 => 'qcloudsms',
+    ),
+    'sms_check' => 
+    array (
+      0 => 'qcloudsms',
+    ),
+  ),
+  'route' => 
+  array (
+    '/invite/[:id]$' => 'invite/index/index',
+    '/leesign$' => 'leesign/index/index',
+    '/third$' => 'third/index/index',
+    '/third/connect/[:platform]' => 'third/index/connect',
+    '/third/callback/[:platform]' => 'third/index/callback',
+  ),
+);
---
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:1:{s:11:"leesignhook";a:1:{i:0;s:23:"\addons\leesign\Leesign";}}
